8a2795c653b471a2cb64830ad7fa6e6e271c4742
OtntO123 <31671414+OtntO123@users.noreply.github.com>
2018-12-24 00:47:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,8 @@
             }
         </script>
 
-        <?php include 'message.php'; ?>
+        
+        <br>
+        <p>感谢所有为CSSA做出贡献的同学，本网页由Jonnyy，OtntO，SCW提供技术支持<br><br>隐私声明：本网站不使用数据库，不记录任何数据</p>
     </body>
 </html>
